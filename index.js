093033f4c62d67cd01233f2f7a659718cb0854d8
Damian@Vu <minhvdq@gmail.com>
2023-10-13 14:47:23 -0400

    after database

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
+/*
+//using Mongo
+const mongoose = require('mongoose')
+
+const password = "minhvdq3008"
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.vzqds2z.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('toJSON',{
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString
+    console.log(typeof(ret._id))
+    console.log(ret._id)
+    delete ret._id
+    delete ret.__v;
+  }
+})
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+Note.find({}).then(notes => {
+   notes.map(note => console.log(`${note.name}`))
+})
+*/
+
+//using Dotenv
+require('dotenv').config()
+
+//Importing the note module
+const Note = require('./models/note')
+
 const express = require('express')
 const cors = require('cors')
 
 const app = express()
 app.use(cors())
 app.use(express.static('dist'))
+
+app.use(express.json())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -16,35 +55,12 @@ const requestLogger = (request, response, next) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
-app.use(express.json())
 app.use(requestLogger)
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
 })
 
 const generateId = () => {
@@ -63,41 +79,58 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
+    important: body.important || false
+  })
+  note.save().then(savedNote => {
+      response.json(savedNote)
+    })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-
-  response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id).then(note => {
+    if(note){
+      response.json(note)
+    }
+    else{
+      response.status(400).end()
+    }
+  }).catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
 })
 
-app.use(unknownEndpoint)
+app.put('/api/notes/:id', (request, response, next) => {
+  const bod = request.body
+  const note = {
+    content: bod.content,
+    important: bod.important
+  }
+  
+  Note.findByIdAndUpdate(request.params.id, note, {new: true}).then(
+    updatedNote => response.json(updatedNote)
+  ).catch(error => next(error))
+})
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (err, req, res, next) => {
+  console.log(err.messsage)
+
+  if(err.name === 'CastError'){
+    return res.status(400).send({ error: 'malformatted id '})
+  }
+  next(error)
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.vzqds2z.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+
+const note = new Note({
+  content: "HTML is easy",
+  important: true
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  console.log(result)
+  mongoose.connection.close()
+})
+
+/*
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
+*/
